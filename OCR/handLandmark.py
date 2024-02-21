7577270921419784434250843a8330a9eb73941b
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-02-22 02:37:54 +0900

    fix: update num_hands value

--- OCR/handLandmark.py
+++ OCR/handLandmark.py
@@ -56,7 +56,7 @@ def read_image_from_uri(uri):
 def get_finger_coordinate(uri):
     base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
     options = vision.HandLandmarkerOptions(base_options=base_options,
-                                            num_hands=2)
+                                            num_hands=1)
     detector = vision.HandLandmarker.create_from_options(options)
 
     # STEP 3: Load the input image.
--- OCR/main.py
+++ OCR/main.py
@@ -30,7 +30,6 @@ async def image_to_text(image: Image):
 # async def image_to_text(pointer: Pointer):
 # 	return googleOCR.text_pointer(pointer.imageUrl, pointer.x, pointer.y)
 
-
 @app.post("/image/pointer")
 async def image_to_text(pointer: Pointer):
 	return handLandmark.text_pointer(pointer.imageUrl)
